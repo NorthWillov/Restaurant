8347b176d97c41d3df363e8c1322d09ed883684c
NorthWillov <northwillov@gmail.com>
2021-05-06 13:10:16 +0200

    Fix index.tsx imports

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -74,7 +74,10 @@ module.exports = {
             presets: [
               "@babel/preset-env",
               "@babel/preset-react",
-              "@babel/preset-typescript",
+              [
+                "@babel/preset-typescript",
+                { isTSX: true, allExtensions: true },
+              ],
             ],
           },
         },
