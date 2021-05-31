930e36dc326b9053ad40cf910f2f3cd19b25aaeb
Artem <northwillov@gmail.com>
2021-05-31 16:25:17 +0200

    Add lunches state

--- server/index.js
+++ server/index.js
@@ -58,7 +58,7 @@ app.get("/api/getPromos", async (req, res) => {
   });
 });
 
-app.get("/api/getLunches", async (req, res) => {
+app.get("/api/fetchLunches", async (req, res) => {
   await Lunch.find((err, lunches) => {
     if (err) return console.log(err);
     res.json(lunches);
