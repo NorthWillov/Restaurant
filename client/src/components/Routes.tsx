ad42bff6fb89d3daab373e41d8a4984fd73f7c1e
NorthWillov <northwillov@gmail.com>
2022-02-11 13:22:21 +0100

    Add lazy components

--- client/src/components/Routes.tsx
+++ client/src/components/Routes.tsx
@@ -1,31 +1,35 @@
-import React, { FC } from "react"
+import React, { FC, Suspense, lazy } from "react"
 import { Switch, Route, Redirect } from "react-router-dom"
-import LandingPage from "./pages/LandingPage"
-import CartPage from "./pages/CartPage"
-import AdminPage from "./pages/AdminPage"
-import DeliveryFormPage from "./pages/DeliveryFormPage"
-import ThanksPage from "./pages/ThanksPage"
+import { Spinner } from "react-bootstrap"
+
+const LandingPage = lazy(() => import("./pages/LandingPage"))
+const CartPage = lazy(() => import("./pages/CartPage"))
+const AdminPage = lazy(() => import("./pages/AdminPage"))
+const DeliveryFormPage = lazy(() => import("./pages/DeliveryFormPage"))
+const ThanksPage = lazy(() => import("./pages/ThanksPage"))
 
 const Routes: FC = () => {
   return (
-    <Switch>
-      <Route exact path="/">
-        <LandingPage />
-      </Route>
-      <Route exact path="/admin">
-        <AdminPage />
-      </Route>
-      <Route exact path="/cart">
-        <CartPage />
-      </Route>
-      <Route exact path="/cart/delivery-info">
-        <DeliveryFormPage />
-      </Route>
-      <Route exact path="/thanks">
-        <ThanksPage />
-      </Route>
-      <Redirect to="/" />
-    </Switch>
+    <Suspense fallback={<Spinner animation="border" variant="success" />}>
+      <Switch>
+        <Route exact path="/">
+          <LandingPage />
+        </Route>
+        <Route exact path="/admin">
+          <AdminPage />
+        </Route>
+        <Route exact path="/cart">
+          <CartPage />
+        </Route>
+        <Route exact path="/cart/delivery-info">
+          <DeliveryFormPage />
+        </Route>
+        <Route exact path="/thanks">
+          <ThanksPage />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    </Suspense>
   )
 }
 
