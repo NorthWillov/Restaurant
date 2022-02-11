b746e0fe5c3eeb601be315cf6378eb542f7354a3
NorthWillov <northwillov@gmail.com>
2022-02-11 15:43:21 +0100

    Add ErrorBoundary to routes

--- client/src/components/Routes.tsx
+++ client/src/components/Routes.tsx
@@ -1,6 +1,7 @@
 import React, { FC, Suspense, lazy } from "react"
 import { Switch, Route, Redirect } from "react-router-dom"
 import { Spinner } from "react-bootstrap"
+import ErrorBoundary from "./ErrorBoundary"
 
 const LandingPage = lazy(() => import("./pages/LandingPage"))
 const CartPage = lazy(() => import("./pages/CartPage"))
@@ -10,26 +11,28 @@ const ThanksPage = lazy(() => import("./pages/ThanksPage"))
 
 const Routes: FC = () => {
   return (
-    <Suspense fallback={<Spinner animation="border" variant="success" />}>
-      <Switch>
-        <Route exact path="/">
-          <LandingPage />
-        </Route>
-        <Route exact path="/admin">
-          <AdminPage />
-        </Route>
-        <Route exact path="/cart">
-          <CartPage />
-        </Route>
-        <Route exact path="/cart/delivery-info">
-          <DeliveryFormPage />
-        </Route>
-        <Route exact path="/thanks">
-          <ThanksPage />
-        </Route>
-        <Redirect to="/" />
-      </Switch>
-    </Suspense>
+    <ErrorBoundary>
+      <Suspense fallback={<Spinner animation="border" variant="success" />}>
+        <Switch>
+          <Route exact path="/">
+            <LandingPage />
+          </Route>
+          <Route exact path="/admin">
+            <AdminPage />
+          </Route>
+          <Route exact path="/cart">
+            <CartPage />
+          </Route>
+          <Route exact path="/cart/delivery-info">
+            <DeliveryFormPage />
+          </Route>
+          <Route exact path="/thanks">
+            <ThanksPage />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </Suspense>
+    </ErrorBoundary>
   )
 }
 
